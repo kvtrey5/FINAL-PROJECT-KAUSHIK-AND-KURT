18ad953a960df182eb0eebbfb9cc066b0c23a77a
XCKurt <168512138+XCKurt@users.noreply.github.com>
2024-05-01 18:34:31 -0700

    Update gameplay.h

--- gameplay.h
+++ gameplay.h
@@ -1,21 +1,63 @@
 #pragma once
 #include <iostream>
 #include "PLAYERDRAFT.h"
+#include "Player.h"
 
-using namespace std; 
+using namespace std;
+
 
 int teamscore;
 int opponentscore;
+int i;
+string playername;
 
-
-void offensivetwo()
+int main()
 {
-	int twodecider = ((rand() % 30) + 1);
+	teamscore = 0;
+	cout << "Enter Player Name";
+	cin >> playername;
 
-	if (twodecider = < scoring)
-	{
-		cout << playername << " hit a layup!" << endl;
-		++teamscore;
-	}
 	
+	Player();
+	Defender();
+
+	void offensivetwo();
+	{
+		int twodecider = ((rand() % 30) + 1);
+
+		if (twodecider = < scoring)
+		{
+			cout << playername << " hit a layup!" << endl;
+			++teamscore;
+		}
+
+	}
+	void offensivethree();
+	{
+		int threedecider = ((rand() % 50) + 1);
+
+		if (threedecider = < scoring)
+		{
+			cout << playername << " hit a three!" << endl;
+			++teamscore;
+		}
+
+	}
+	else
+	{
+		void offensiverebound();
+		{
+			int rebound = ((rand() % 10) + 1);
+
+			if (rebound = < scoring)
+			{
+				cout << playername << " your player got a rebound, time for a seconod chance!" << endl;
+				++teamscore;
+			}
+
+		}
+	}
+
+
+
 }
